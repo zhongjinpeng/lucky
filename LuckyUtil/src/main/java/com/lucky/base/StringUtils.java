402ccaa1795738c6e71eb3fd20da31931eddea33
zhongjinpeng <2498305587@qq.com>
2020-08-17 17:02:00 +0800

    基于mybatis+mysql实现水平分表

--- LuckyUtil/src/main/java/com/lucky/base/StringUtils.java
+++
@@ -1,9 +0,0 @@
-package com.lucky.base;
-
-/**
- * @author <a href="mailto:jpzhong1994@gmail.com">zhongjinpeng</a>
- * @version 1.0
- * @date 2020/6/29 11:26
- */
-public class StringUtils extends org.apache.commons.lang.StringUtils {
-}
